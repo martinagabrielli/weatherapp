b9badca5c76c1428dbb7157ef1f3d9feac2b7aa1
Martina <martina.gabrielli@yahoo.it>
2019-10-09 20:17:11 +0100

    added 'load' event listener, geolocation, api key

---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', ()=>{
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+
+    
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position=>{
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = "https://cors-anywhere.herokuapp.com";
+            const api = `${proxy}https://api.darksky.net/forecast/b1f70db6725238c6a29e9dff6e830451/${lat},${long}`;
+
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const {temperature, summary} = data.currently;
+            });
+        });
+    }
+});
